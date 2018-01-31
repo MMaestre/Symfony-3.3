541c73be1bf679a1fcffbbd0022ca0dffdff1da9
María Maestre <maria.maestre@pccomponentes.com>
2018-01-31 15:28:59 +0100

    Form Controller => Uso de Doctrine, Repositorios y Formularios

--- proyectoPrueba/README.md
+++ proyectoPrueba/README.md
@@ -3,5 +3,10 @@ proyectoPrueba
 
 **January 31, 2018, 12:31 pm.** <br/>
 * Creación del proyecto.
-* Creación de Controller básico de prueba
 * Creación de Bundle básico
+  * Creación de Controller básico -> LuckyNumberController
+  * Utilización de template en el Controller.
+* Unión con Doctrine
+  * Creación de BB.DD. -> test_project.
+  * Creación de Entidad -> Product.
+  * Creación de Repositorio -> ProductRepository.
--- proyectoPrueba/src/AppBundle/Controller/FormController.php
+++ proyectoPrueba/src/AppBundle/Controller/FormController.php
@@ -4,19 +4,24 @@ namespace AppBundle\Controller;
 
 use AppBundle\Entity\Product;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\HttpFoundation\Request;
 
-class DefaultController extends Controller
+class FormController extends Controller
 {
-    public function newProduct (Request $request)
+    /**
+     * @Route("/nuevo/producto")
+     */
+    public function newProductAction (Request $request)
     {
-        //Crea un nuevo producto
+        /** PRODUCTO EJEMPLO PA COMPROBAR FORMULARIO
+         * $product = new Product();
+         * $product->setName('GHB Sades Auriculares Gaming Cascos con Microfono SA-901 Sonido Envolvente 7.1 con USB para PC Ordenador Portátil Azul y Negro');
+         * $product->setPrice(21.99); */
         $product = new Product();
-        $product->setName('GHB Sades Auriculares Gaming Cascos con Microfono SA-901 Sonido Envolvente 7.1 con USB para PC Ordenador Portátil Azul y Negro');
-        $product->setPrice(21.99);
 
         //Crea el formulario
         $form= $this->createFormBuilder($product)
@@ -25,6 +30,21 @@ class DefaultController extends Controller
             ->add('save', SubmitType::class, array('label' => 'Guardar'))
             ->getForm();
 
-        return $this->render('default/form.html.twig', array('form'=>$form->createView(),));
+        //Gestionar respueta
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $product = $form->getData();
+
+            // Se guardan los datos en la base de datos
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($product);
+            $em->flush();
+
+            return $this->redirectToRoute('form_success');
+        }
+
+        return $this->render('default/form.html.twig', array(
+            'form' => $form->createView(),));
     }
 }
